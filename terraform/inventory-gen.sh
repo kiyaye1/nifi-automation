fba62355a2cd6160877c0695747e48ea894fb181
kiyaye1 <36045045+kiyaye1@users.noreply.github.com>
2025-07-31 14:08:50 -0400

    Update inventory-gen.sh

--- terraform/inventory-gen.sh
+++ terraform/inventory-gen.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+set -e
+
 IP=$(terraform -chdir=terraform output -raw instance_public_ip)
 
-cat <<EOF > ../ansible/inventory.ini
-[nifi]
-$IP ansible_user=ubuntu ansible_ssh_private_key_file=__PLACEHOLDER__
-EOF
+# Ensure ansible directory exists
+mkdir -p ../ansible
+
+# Write inventory
+echo "[nifi]" > ../ansible/inventory.ini
+echo "$IP ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/nifi.pem" >> ../ansible/inventory.ini
+
